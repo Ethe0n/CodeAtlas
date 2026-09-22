946396858211ae34e418f29de12ccbf872fc3ef1
Mingyu Kim <mingyu.kim@knu.ac.kr>
2026-09-22 21:09:13 +0900

    sample project .net v4.5.2

---
+++ sample_project_net452/My Project/AssemblyInfo.vb
@@ -0,0 +1,12 @@
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyTitle("sample_project_net452")>
+<Assembly: AssemblyDescription("VB.NET Framework 4.5.2 sample project")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("sample_project_net452")>
+<Assembly: ComVisible(False)>
+<Assembly: Guid("87E2DC7D-0DC0-45F9-8E11-D82AEAF1027F")>
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
